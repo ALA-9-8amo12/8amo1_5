cc6a81c9772eb80fa726a6849f9c42910a52f4b3
SanderGrimmelikhuizen <48210381+SanderGrimmelikhuizen@users.noreply.github.com>
2020-11-22 21:57:04 +0100

    Geen idee wat er fout gaat
    Ben bezig met de splashscreen alleen ik heb meerdere tutorials gebruikt maar ik krijg alleen maar errors en fouten. Dat dat vermeenvuldigd zichzelf heel de tijd.

--- app/src/main/java/com/example/project_amazigh/MainActivity.java
+++ app/src/main/java/com/example/project_amazigh/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.project_amazigh;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Intent intent =  new Intent(this, Splashscreen.class);
+        startActivity(Intent);
+        finish(Intent);
     }
 }
