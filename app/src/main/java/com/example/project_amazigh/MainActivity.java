86af7a61eabba3b0cefddded63596b7b434b0b16
vanLieropFabian8amo1 <1043972@mborijnland.com>
2020-11-23 09:46:02 +0100

    splash screen af

--- app/src/main/java/com/example/project_amazigh/MainActivity.java
+++ app/src/main/java/com/example/project_amazigh/MainActivity.java
@@ -4,24 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener {
-    private View btnOK;
-
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnOK=(View) findViewById(R.id.view_to_listen_for_touch);
-        btnOK.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent menu = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(menu);
+                finish();
+            }
+        }, SPLASH_TIME_OUT );
     }
 }
