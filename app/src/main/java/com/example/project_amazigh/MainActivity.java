2b5cf45cab5a98da1fe1b6e74f289ea87e86d1f0
SanderGrimmelikhuizen <48210381+SanderGrimmelikhuizen@users.noreply.github.com>
2020-11-23 09:28:42 +0100

    Voor Fabian

--- app/src/main/java/com/example/project_amazigh/MainActivity.java
+++ app/src/main/java/com/example/project_amazigh/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.project_amazigh;
 
-
-import android.content.Intent;
 import android.os.Bundle;
 
-public class Splashscreen extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent =  new Intent(this, Splashscreen.class);
-        startActivity(Intent);
-        finish(Intent);
+        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/project_amazigh/SplashScreen.java
+++ app/src/main/java/com/example/project_amazigh/SplashScreen.java
@@ -2,30 +2,15 @@ package com.example.project_amazigh;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashScreenActivity extends AppCompatActivity {
-
-    private static int SPLASH_TIME_OUT = 3000;
-
+public class SplashScreen extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
-
+//We will use the following code to load Splash Screen first then move it to Main Activity
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
